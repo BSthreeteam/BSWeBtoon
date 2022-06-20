8cfa5b830f81d7fb61c6a1419ad35af047e149ac
盈均 <lkk975038781@gmail.com>
2022-06-20 15:04:55 +0800

    ViewModel

---
+++ BSWeBtoon.Front/ViewModel/WeekViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Mvcdemo.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BSWeBtoon.Front.ViewModel
+{
+    public class WeekViewModel
+    {
+        public string ComicId { get; set; }
+
+        public string ComicName { get; set; }
+        public string NamePic { get; set; }
+        public string BgPic { get; set; }
+        public string PeoplePic { get; set; }
+        public DateTime? LaunchDate { get; set; }
+        public string Introduction { get; set; }
+        public string ComicStatusId { get; set; }
+        public virtual ComicStatus ComicStatus { get; set; }
+
+
+    }
+}
